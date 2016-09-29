cca3ecb93cb97f870f9405423abb90c9b63d79a2
nelitaaas <nelitaapriyani25@gmail.com>
2016-09-29 22:07:39 +0700

    Lanjut Tugas Login

---
+++ Tugas-2/app/src/main/java/com/nelitaaas/tugas_2/MainMenu.java
@@ -0,0 +1,18 @@
+package com.nelitaaas.tugas_2;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by nelitaaas on 29/09/16.
+ */
+
+public class MainMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_menu);
+    }
+}
